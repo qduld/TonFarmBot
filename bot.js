7073347252089b4398ded02c40cac75cb46464e4
lid <lid@keenon.com>
2024-11-09 00:55:25 +0800

    feat: avatar test

--- bot.js
+++ bot.js
@@ -1,4 +1,6 @@
 "use strict";
+// 现在，你已经确定了将如何处理信息，可以开始运行你的 bot。
+// 这将连接到 Telegram 服务器并等待消息。
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -10,18 +12,34 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const grammy_1 = require("grammy");
-const bot = new grammy_1.Bot("7360724156:AAGeBGUrfDuRRYTkL-G4ZWKmi3rIKWH05VU"); // <-- 把你的 bot token 放在 "" 之间
-// 你现在可以在你的 bot 对象 `bot` 上注册监听器。
-// 当用户向你的 bot 发送消息时，grammY 将调用已注册的监听器。
-const GAME_SHORT_NAME = "menghuan";
-// 处理 /start 命令。
-bot.command("start", (ctx) => __awaiter(void 0, void 0, void 0, function* () { return yield ctx.replyWithGame(GAME_SHORT_NAME); }));
-// 处理其他的消息。
+const bot = new grammy_1.Bot("7360724156:AAGeBGUrfDuRRYTkL-G4ZWKmi3rIKWH05VU"); // <-- 替换为你的 bot token
+const GAME_SHORT_NAME = "menghuan"; // 游戏的短名称
+const GAME_URL = "http://3.25.96.209/farm/"; // Web App 的 URL
+// 处理 /start 命令，启动游戏
+// 处理 /start 命令，启动游戏并带有按钮
+bot.command("start", (ctx) => __awaiter(void 0, void 0, void 0, function* () {
+    yield ctx.reply("欢迎来到游戏！点击下面的按钮开始游戏。", {
+        reply_markup: {
+            inline_keyboard: [
+                [
+                    {
+                        text: "开始游戏",
+                        web_app: {
+                            url: GAME_URL, // 当按钮被点击时打开的 Web App URL
+                        },
+                    },
+                ],
+            ],
+        },
+    });
+}));
+// 处理其他消息
 bot.on("message", (ctx) => ctx.reply("Got another message!"));
-const GAME_URL = "http://3.25.96.209/farm/";
+// 处理游戏回调查询
 bot.on("callback_query:game_short_name", (ctx) => __awaiter(void 0, void 0, void 0, function* () {
-    console.log("ctx.callbackQuery.game_short_name = ", ctx.callbackQuery.game_short_name);
+    console.log("Received callback query:", ctx.callbackQuery.game_short_name);
     if (ctx.callbackQuery.game_short_name === GAME_SHORT_NAME) {
+        // 当回调查询的游戏名称匹配时，打开 Web App URL
         yield ctx.answerCallbackQuery({ url: GAME_URL });
     }
 }));
@@ -33,7 +51,5 @@ bot.on("callback_query:game_short_name", (ctx) => __awaiter(void 0, void 0, void
 //       { parse_mode: "HTML" }
 //     )
 // );
-// 现在，你已经确定了将如何处理信息，可以开始运行你的 bot。
-// 这将连接到 Telegram 服务器并等待消息。
-// 启动 bot。
+// 启动 bot，等待消息和回调查询
 bot.start();
